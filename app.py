7795ae911097a53fc7a0b944f4da33fe5bc9c2b5
Ruben Raya Delgado <rraya@ndscognitivelabs.com>
2021-10-13 13:31:02 -0500

    cluster

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import json
-from flask import Flask, render_template, jsonify
+from flask import Flask, redirect, url_for, request, render_template
 import pymongo
 from decouple import config
 from bson.json_util import dumps
@@ -11,17 +10,69 @@ app = Flask(__name__)
 
 client = pymongo.MongoClient(SECRET_KEY)
 db = client.Escuela
-usuarios = db.alumno
+cuentas = db.alumno
 
 
 @app.route('/')
 def index():
+    return render_template("/LoginFull.html")
 
-    _cursor = usuarios.find({})
-    list_cur = list(_cursor)
-    json_data = dumps(list_cur)
-    
-    return json_data
+
+@app.route('/insert', methods=["POST"])
+def insert():
+
+    user = {
+        "matricula": request.form['matricula'],
+        "nombre": request.form['nombre'],
+        "correo": request.form['correo'],
+        "contrasena": request.form['contrasena']
+    }
+    try:
+        cuentas.insert_one(user)
+        return redirect(url_for('find_all'))
+
+    except Exception as e:
+        return '<p>Unexpected error: %s %s </p>' % type(e), e
+
+
+@app.route('/update', methods=['POST'])
+def update():
+
+    try:
+        filter = {"matricula": request.form['matricula']}
+        user = {"$set": {
+            "nombre": request.form['nombre'], "correo": request.form['correo'], "contrasena": request.form['contrasena']}}
+        cuentas.update_one(filter, user)
+        return redirect(url_for('find_all'))
+
+    except Exception as e:
+        return '<p>Unexpected error: %s %s </p>' % type(e), e
+
+
+@app.route('/find_one/<matricula>')
+def find_one(matricula):
+    cursor = cuentas.find_one({"id": (matricula)})
+    return '<b>find : %s!</b>' % (cursor)
+
+
+@app.route('/delete/<matricula>')
+def delete(matricula):
+    cursor = cuentas.delete_one({'matricula': (matricula)})
+    if cursor.deleted_count == 0:
+        return '<b>El registro no esta...!!</b>'
+    else:
+        return redirect(url_for('find_all'))
+
+
+@app.route('/find_all')
+def find_all():
+    cursor = cuentas.find({})
+    user = []
+    for doc in cursor:
+        user.append(doc)
+
+    return render_template("/Retrieve.html", data=user)
+    # return '<b>Nombre: %s!</b>' % (user.nombre)
 
 
 if __name__ == '__main__':
