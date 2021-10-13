0834fb069b3da19a53d1c998a34b0d3d6a55c25d
Ruben Raya Delgado <rraya@ndscognitivelabs.com>
2021-10-13 12:51:55 -0500

    cluster

--- app.py
+++ app.py
@@ -2,19 +2,15 @@ from flask import Flask, render_template
 import pymongo
 
 app = Flask(__name__)
+client = pymongo.MongoClient(
+    "mongodb+srv://rraya:rubenraya@cluster0.w9ojs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 
 @app.route('/')
 def index():
-    client = pymongo.MongoClient(
-    "mongodb+srv://rraya:rubenraya@cluster0.w9ojs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    db = client.Escuela
-    usuarios = db.alumno
 
     return render_template('index.html')
 
 
-
-
-
-if __name__ == '__main__': app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
