988fe0342e92c4c216071002cb6bbeb25d8a0172
Ruben Raya Delgado <rraya@ndscognitivelabs.com>
2021-10-13 13:11:08 -0500

    cluster

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, jsonify
 import pymongo
 from decouple import config
+from bson.json_util import dumps
 
 config.encoding = 'cp1251'
 SECRET_KEY = config('SECRET_KEY')
 
 app = Flask(__name__)
 
+client = pymongo.MongoClient(SECRET_KEY)
+db = client.Escuela
+usuarios = db.alumno
+
 
 @app.route('/')
 def index():
 
-    client = pymongo.MongoClient(SECRET_KEY)
-    db = client.Escuela
-    usuarios = db.alumno
-    return usuarios
+    _cursor = usuarios.find({})
+    list_cur = list(_cursor)
+    json_data = dumps(list_cur)
+    
+    return json_data
 
 
 if __name__ == '__main__':
